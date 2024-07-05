a594ba8cb3a6c2c2625cc4882bad718de5c6f0cf
dinukah99 <dinukahamarasinghe@gmail.com>
2024-07-05 23:59:26 +0530

    fix issue with custom error middleware

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import { notFound, error } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 5000;
 import userRoutes from './routes/userRoutes.js';
 
@@ -9,7 +9,7 @@ const app = express();
 
 app.use('/api/users', userRoutes);
 app.use(notFound);
-app.use(errorHandler);
+app.use(error);
 
 app.get('/', (req, res) => {
     res.send("Server is ready!");
