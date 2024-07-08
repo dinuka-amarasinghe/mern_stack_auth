5d63c23f10919c125e088083cf5c43afbd5c92f7
dinukah99 <dinukahamarasinghe@gmail.com>
2024-07-08 20:31:55 +0530

    user logout functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,7 @@ import generateToken from '../utils/generateToken.js';
 // route POST /api/users/auth
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
     const user = await User.findOne({ email: email });
 
     if (user && (await user.matchPassword(password))) {
@@ -58,7 +58,11 @@ const registerUser = asyncHandler(async (req, res) => {
 // route POST /api/users/logout
 // @access Public
 const logoutUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Logout User' });
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    });
+    res.status(200).json({ message: 'User Logged Out!' });
 });
 
 // @desc Get user profile
