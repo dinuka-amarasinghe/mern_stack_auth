5919d4d3b777c0e9f0e340365c43f693441a7327
dinukah99 <dinukahamarasinghe@gmail.com>
2024-07-18 23:53:38 +0530

    add production setup to serve frontend build files

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { notFound, error } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 const port = process.env.PORT || 5000;
 import userRoutes from './routes/userRoutes.js';
+import path from 'path';
 
 connectDB();
 
@@ -18,9 +19,17 @@ app.use('/api/users', userRoutes);
 app.use(notFound);
 app.use(error);
 
-app.get('/', (req, res) => {
-    res.send("Server is ready!");
-});
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, 'frontend/dist')));
+    
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 
+        'index.html')));
+} else {
+    app.get('/', (req, res) => {
+        res.send("Server is ready!");
+    });
+}
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
